f3bef712432da2e79babd102d689a066c593ce08
I'm Batman <auns.mehmood@consultancyoutfit.co.uk>
2025-10-27 10:18:46 +0500

    folder structure done

---
+++ src/assets/index.tsx
@@ -0,0 +1,13 @@
+// image onebackroute_oneImg.js
+export { default as CqcFundamentalStandardsImage } from "./CqcFundamentalStandardsImage.svg";
+
+// image produc_onebackroute_oneImg.js
+export { default as mainTestLogo } from "./mainTestLogo.svg";
+
+// images multi-page-scricpt.js
+export { default as img1ScopeOfRegistrationImage } from "./img1ScopeOfRegistrationImage.svg";
+export { default as img2ScopeOfRegistrationImage } from "./img2ScopeOfRegistrationImage.svg";
+export { default as img3ScopeOfRegistrationImage } from "./img3ScopeOfRegistrationImage.svg";
+
+
+
